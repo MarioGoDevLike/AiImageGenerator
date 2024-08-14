713318bab36531b83db50dde505b2a4a350f0984
MarioGoDevLike <marionassar57@gmail.com>
2024-08-15 01:11:42 +0300

    finalyzing the design

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -4,7 +4,7 @@ const FormField = ({labelName, type, name, placeholder,value,handleChange, isSur
   return (
     <div>
       <div className='flex items-center gap-2 mb-2'>
-        <label htmlFor={name} className='block text-sm font-medium text-gray-900'>
+        <label htmlFor={name} className='block text-sm font-medium text-[#E2E2B6]'>
           {labelName}
         </label>
         {isSurpriseMe && (
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -81,9 +81,9 @@ const CreatePost = () => {
   return (
     <section className="max-w-7xl mx-auto">
       <div>
-        <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
+        <h1 className="font-extrabold text-[#E2E2B6] text-[32px]">Create</h1>
         <p className="mt-2 text-[#666e75] text-[16px] max-w[500px]">
-          Create imaginative and visually stunning images through by DALL-E AI
+          Create imaginative and visually stunning images through by Pollinations AI
           and share it with the Community
         </p>
         <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
