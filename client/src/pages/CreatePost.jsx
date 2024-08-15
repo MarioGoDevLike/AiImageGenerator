3d5c0e3f944805bd402146fc7e8e6e2c0bbc1d6b
MarioGoDevLike <marionassar57@gmail.com>
2024-08-15 12:10:21 +0300

    updating endpoints

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -20,7 +20,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
+        const response = await fetch("https://generate-an-image.vercel.app/api/v1/post", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(form),
@@ -47,7 +47,7 @@ const CreatePost = () => {
       try {
         setGeneratingImg(true);
         const response = await fetch(
-          "http://localhost:8080/imagine/api/generations/",
+          "https://generate-an-image.vercel.app/imagine/api/generations/",
           {
             method: "POST",
             headers: {
