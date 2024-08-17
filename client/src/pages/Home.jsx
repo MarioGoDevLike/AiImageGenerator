d4a46dc1285b3fd57cc408da2d3c74963061c7fa
MarioGoDevLike <marionassar57@gmail.com>
2024-08-17 15:49:25 +0300

    fixing favicon.ico issue

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
     const fetchPosts = async () => {
       setLoading(true);
       try {
-        const response = await fetch("https://generate-an-image/api/v1/post", {
+        const response = await fetch("https://generate-an-image.vercel.app/api/v1/post", {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
