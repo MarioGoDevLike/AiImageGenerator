7a5ee40916e9780bc5e5db83a1f6ed6ecf832118
MarioGoDevLike <marionassar57@gmail.com>
2024-08-17 14:50:04 +0300

    removed unecessary import

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { preview } from "../assets";
 import { getRandomPrompt } from "../utils";
-import { FormField, Card, Loader } from "../components";
+import { FormField, Loader } from "../components";
 
 const CreatePost = () => {
   const navigate = useNavigate();
